c8912396ca66526878025c6727cee8e416f3412d
Guillaume <michaudg@gmail.com>
2016-07-23 16:13:40 -0400

    Removed spaces

--- godaddy.sh
+++ godaddy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 # GoDaddy.sh v1.0 by Nazar78 @ TeaNazaR.com
 ###########################################
 # Simple DDNS script to update GoDaddy's DNS. Just schedule every 5mins in crontab.
@@ -11,50 +11,50 @@
 #
 # History:
 # v1.0 - 20160513 - 1st release.
-# 
+#
 # PS: Feel free to distribute but kindly retain the credits (-:
 ###########################################
- 
+
 # Begin settings
 # Get the Production API key/secret from https://developer.godaddy.com/keys/.
 # Ensure it's for "Production" as first time it's created for "Test".
 Key=<API production key>
 Secret=<API secret>
- 
+
 # Domain to update.
 Domain=<domain name>
- 
+
 # Advanced settings - change only if you know what you're doing :-)
 # Record type, as seen in the DNS setup page, default A.
 Type=A
- 
+
 # Record name, as seen in the DNS setup page, default @.
 Name=@
- 
+
 # Time To Live in seconds, minimum default 600 (10mins).
 # If your public IP seldom changes, set it to 3600 (1hr) or more for DNS servers cache performance.
 TTL=600
- 
+
 # Writable path to last known Public IP record cached. Best to place in tmpfs.
 CachedIP=/tmp/current_ip
- 
+
 # External URL to check for current Public IP, must contain only a single plain text IP.
 # Default http://api.ipify.org.
 CheckURL=http://api.ipify.org
- 
+
 # Optional scripts/programs/commands to execute on successful update. Leave blank to disable.
 # This variable will be evaluated at runtime but will not be parsed for errors nor execution guaranteed.
 # Take note of the single quotes. If it's a script, ensure it's executable i.e. chmod 755 ./script.
 # Example: SuccessExec='/bin/echo "$(date): My public IP changed to ${PublicIP}!">>/var/log/GoDaddy.sh.log'
 SuccessExec=''
- 
+
 # Optional scripts/programs/commands to execute on update failure. Leave blank to disable.
 # This variable will be evaluated at runtime but will not be parsed for errors nor execution guaranteed.
 # Take note of the single quotes. If it's a script, ensure it's executable i.e. chmod 755 ./script.
 # Example: FailedExec='/some/path/something-went-wrong.sh ${Update} && /some/path/email-script.sh ${PublicIP}'
 FailedExec=''
 # End settings
- 
+
 Curl=$(/usr/bin/which curl 2>/dev/null)
 Touch=$(/usr/bin/which touch 2>/dev/null)
 [ "${Curl}" = "" ] &&
@@ -102,7 +102,7 @@ if [ "$(cat ${CachedIP} 2>/dev/null)" != "${PublicIP}" ];then
       echo "Fail! ${Update}"
       eval ${FailedExec}
       exit 1
-    fi  
+    fi
   fi
 else
   echo "Current 'Public IP' matches 'Cached IP' recorded. No update required!"
